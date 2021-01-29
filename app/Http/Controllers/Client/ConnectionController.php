13f83a199b3eecc66af4856dcb16913d27229624
Luan Pham <luanpm88@gmail.com>
2021-01-29 11:29:28 +0700

    fix bug

--- app/Http/Controllers/Client/ConnectionController.php
+++ app/Http/Controllers/Client/ConnectionController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
 use App\Jobs\LazadaSync;
+use App\Library\Lazada\LazadaConnection;
 
 class ConnectionController extends Controller
 {
