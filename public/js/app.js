e64484e03b74a05181c7bc4adad7f7c8d625eab0
Luan Pham <luanpm88@gmail.com>
2021-02-03 23:59:48 +0700

    broadcast use Pusher

--- public/js/app.js
+++ public/js/app.js
@@ -43427,12 +43427,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "123",
-  cluster: "mt1",
-  wsHost: window.location.hostname,
-  wsPort: 6001,
-  forceTLS: false,
-  disableStats: true
+  key: "eca6730fef82065e2143",
+  cluster: "ap1",
+  forceTLS: true
 });
 
 /***/ }),
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -37,8 +37,5 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: false,
-    disableStats: true,
+    forceTLS: true
 });
